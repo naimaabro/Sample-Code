dbcff390b431df98c364b6fbf16b565f25ab458b
naimaabro <naimaabro@users.noreply.github.com>
2020-06-21 07:30:44 +0000

    automated commit by check50 [check50=True]

--- credit.c
+++ credit.c
@@ -1,89 +1,138 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 #include <math.h>
 
-int main (void)
+int main(void)
+
 {
-    long card;
+    long Card_Number;
+    int Digit_Number = 0, Current_Digit = 0, Even_x2_Product = 0, Even_Digits = 0, Odd_Digits = 0,
+        Total_Digit_Sum = 0;
+    bool is_even = false;
+
+// Prompt User for Credit Card Number
+
     do
     {
-        card = get_long("Number: ");
+        Card_Number = get_long("Card Number: ");
     }
-    while (card < 1 || card > 9999999999999999);
+    while (Card_Number < 0);
 
+// Check First Digits of Number
 
-    long long temp_num = card ;
-    int rem = 0 ;
-    int count = 0 ;
-    int sum_a = 0 ;
-    int sum_b = 0 ;
+    int Digits_MstrCrd = Card_Number / pow(10, 14);
+    int Digits_Visa_16 = Card_Number / pow(10, 15);
+    int Digits_AmEx = Card_Number / pow(10, 13);
+    int Digits_Visa_13 = Card_Number / (pow(10, 12));
 
+// Loop to determine identity of each digit
 
-    while (card >= 0)
+    while (Card_Number != 0)
     {
-        rem = card % 10 ;
-        count++ ;
-        temp_num = (card - rem) / 10 ;
-        break ;
+        // Get Last Digit of Number
 
+        Current_Digit = (Card_Number % 10);
 
-        for (count = 0 ; count % 2 == 0 ; count++)
+        // Increase Digit Number by 1 
+
+        Digit_Number += 1;
+
+        // Check if Current Digit is at Odd or Even Position in Card Number
+
+        if (is_even == true)
         {
-            sum_a = sum_a + ((card % 10) * 2) ;
+            // Multiply Digit by 2
 
-            while ((card % 10) * 2 >= 10)
+            Even_x2_Product = Current_Digit * 2;
+
+            // Add Digits of Multiplication Product
+
+            while (Even_x2_Product != 0)
             {
-                sum_a = (sum_a % 10) + 1 ;
-            } 
-        }
+                Even_Digits += Even_x2_Product % 10;
+                Even_x2_Product /= 10;
+            }
 
-        for (count = 0 ; count % 2 != 0 ; count++)
+            // Tell Program Next Digit is Odd
+
+            is_even = false; 
+        }
+        else 
         {
-            sum_b = sum_b + rem ;
+            // Add Odd Digits
+
+            Odd_Digits += Current_Digit;
+
+            // Tell Program Next Number is Even 
+
+            is_even = true; 
         }
 
-        return sum_a ;
-        return sum_b ;
-        return count ;
+        // Remove Last Digit and Repeat
+
+        Card_Number /= 10;
     }
 
-    // Checking the validity of the number according to Luhn's algorithm
+    // Add Odd and Even Digits Together
 
-    int total_sum = sum_a + sum_b ;
+    Total_Digit_Sum = Even_Digits + Odd_Digits;
 
-    if (total_sum % 10 != 0)
+// Loop to Check if Card Number is Valid
+
+    if (Total_Digit_Sum % 10 == 0)
     {
-       printf("INVALID\n") ; 
-    }
-    
-    
-    
-    
-    
-    
+        // Check Mastercard
 
+        if (Digit_Number == 16)
+        {
+            if (Digits_MstrCrd <= 55 && Digits_MstrCrd >= 51)
+            {
+                printf("MASTERCARD\n");
+            }
 
+            // Check Visa 16
 
+            else if (Digits_Visa_16 == 4)
+            {
+                printf("VISA\n");
+            }
+            else
+            {
+                printf("INVALID\n");
+            }
+        }
 
-//    {
-  //    card = card %10 ;
-    // }
+        // Check American Express
 
-    if ((card >= 340000000000000 && card <= 349999999999999) || (card >= 370000000000000 && card <= 379999999999999))
-    {
-        printf("AMEX\n");
-    }
-    else if (card >= 5100000000000000 && card <= 5599999999999999)
-    {
-        printf("MASTERCARD\n");
-    }
-    else if ((card >= 4000000000000 && card <= 4999999999999) || (card >= 4000000000000000 && card <= 4999999999999999))
-    {
-        printf("VISA\n");
-    }
-    else
-    {
-        printf("INVALID\n");
+        else if (Digit_Number == 15)
+        {
+            if (Digits_AmEx == 34 || Digits_AmEx == 37)
+            {
+                printf("AMEX\n");
+            }
+            else 
+            {
+                printf("INVALID\n");
+            }
+        }
+
+        // Check Visa 13
+
+        else if (Digit_Number == 13)
+        {
+            if (Digits_Visa_13 == 4)
+            {
+                printf("VISA\n");
+            }
+            else
+            {
+                printf("INVALID\n");
+            }
+        }
+        else 
+        {
+            printf("INVALID\n");
+        }
     }
 }
 
